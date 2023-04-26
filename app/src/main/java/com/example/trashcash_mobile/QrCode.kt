214c34e1875de16ad3eab6ee364a9ff549e3188d
ahmad <a53168@alunos.ipb.pt>
2023-04-26 15:55:43 +0100

    Main activities

---
+++ app/src/main/java/com/example/trashcash_mobile/QrCode.kt
@@ -0,0 +1,11 @@
+package com.example.trashcash_mobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QrCode : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qr_code)
+    }
+}

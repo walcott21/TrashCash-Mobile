d9a9740bb5d978790f2139b90d4bbe61b6d882d1
AymenLaribi <130753603+AymenLaribi@users.noreply.github.com>
2023-06-26 16:07:36 +0100

    the drawer menu is done

--- app/src/main/java/com/example/trashcash_mobile/network/ApiResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.trashcash_mobile.network
-
-import com.google.gson.annotations.SerializedName
-
-data class ApiResponse(
-    @SerializedName("status") val status: String,
-    @SerializedName("message") val message: String? = null,
-    @SerializedName("data") val data: Any? = null
-)
--- app/src/main/java/com/example/trashcash_mobile/network/SignupData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.trashcash_mobile.network
-
-data class SignupData(
-    val name: String,
-    val email: String,
-    val address: String,
-    val password: String,
-    val confirmPassword: String
-)
